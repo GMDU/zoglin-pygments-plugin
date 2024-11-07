3e919a7851c9b2ae10f25fc669c13ec551395bca
Moxvallix Mox <moxvallix@gmail.com>
2024-11-07 17:18:53 +1030

    feat(lexer): improve zoglin lexer

--- zoglin_pygments_plugin.py
+++ zoglin_pygments_plugin.py
@@ -1,6 +1,7 @@
 """Zoglin lexer plugin"""
 
-from pygments.lexer import RegexLexer, words, include
+from pygments.lexer import RegexLexer, words, include, using
+from pygments.lexers.minecraft import MCFunctionLexer
 from pygments.token import Keyword, Text, Comment, Name,\
   Punctuation, Operator, Number, String
 
@@ -14,19 +15,38 @@ class ZoglinLexer(RegexLexer):
     tokens = {
         'root': [
             # Resource location
-            (r'#.+', Comment),
+            (r'#.*', Comment),
             (
-              words(("namespace", "module", "fn",
-                "if", "else", "while", "return", "break", "continue"),
+              words(
+                ("namespace", "module", "fn", "res", "asset"),
+                suffix = r'\b'
+              ),
+              Keyword.Declaration
+            ),
+            (
+              words(
+                ("include", "import", "as", "if", "else", "while", "return", "break", "continue"),
                 suffix = r'\b'
               ),
               Keyword
             ),
             include("literals"),
+            include("commands"),
             (r'(?<=namespace) *\w+', Name.Namespace),
             (r'(?<=module) *\w+', Name.Class),
+            (r'[$&@]?(\w+:|:|~)?[\w\/]+(?=\s*\()', Name.Function),
             (r'[$&@]?(\w+:|:|~)?[\w\/]+:?', Name.Variable),
             (r'[{}()\[\]]', Punctuation),
+        ],
+        "commands": [
+          (
+            r'^\s*(advancement|attribute|bossbar|clear|clone|damage|data|datapack|difficulty|effect|enchant|execute|experience|fill|fillbiome|forceload|function|gamemode|gamerule|give|item|kill|loot|particle|place|playsound|random|recipe|reload|ride|say|schedule|scoreboard|setblock|setworldspawn|spawnpoint|spectate|spreadplayers|stopsound|summon|tag|team|teleport|tellraw|time|title|tp|trigger|weather|worldborder|xp)\b.+',
+            using(MCFunctionLexer)
+          ),
+          (
+            r'(?<=\/).+',
+            using(MCFunctionLexer)
+          )
         ],
         "literals": [
             (r"(true|false)", Keyword.Constant),
